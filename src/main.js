c3ce33d9bddc1209a1ad16fcc60765dcfdaf7072
Daniel Chovanec <chovanec.d2@gmail.com>
2025-01-24 20:28:29 +0000

    Resolve "Support for http request"

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import 'aframe';
+import registerHistogramComponent from "./component/histogram-component.js";
+import initJsroot from "./jsroot/jsroot.js";
+
+initJsroot();
+registerHistogramComponent();
 
 document.querySelector('#app').innerHTML= `
-<a-scene>
-    <a-box position="0 0 -5"></a-box>
+<a-scene id="a-scene">
+    <a-entity histogram></a-entity>
     <a-sky color="#ECECEC"></a-sky>
 </a-scene>
 `;
+
+setTimeout(() => document.querySelector("a-entity").remove(),3000);
---
+++ src/rxjs/subject.js
@@ -0,0 +1,5 @@
+import {ReplaySubject} from "rxjs";
+
+const subject = new ReplaySubject(1);
+
+export default subject;
